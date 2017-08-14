3a4739d4bc6b2c2f01ed96fb0341636ca3f73f5c
Chintan Panchamia <chintan10.panchamia@gmail.com>
2017-08-13 18:08:50 -0700

    resetting project

--- kiosk/auth_data.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'chintanpanchamia'
-
-AUTH_DRCHRONO_KEY = 'FVwFdZegQMaIxG53qC3thUOoCGFKxuuPMW2MoPWq'
-AUTH_DRCHRONO_SECRET = 'Z20YpJkbvHTBpjy46OCy4QUlakYBNaK14JErpZc99JfejIjOhgWZVbdXDfN5EKzjtHJwHdw7Ka3dCmG0sawZC' \
-                              'U7IWSD6KmAzm9JfiZDsccRbsWsPrrtKN0L3mQmN8ejd'
-AUTH_DRCHRONO_SCOPE = ['user:read patients:read calendar:read clinical:read calendar:write clinical:write']
-REDIRECT_URL = 'http://127.0.0.1:8000/index/'
